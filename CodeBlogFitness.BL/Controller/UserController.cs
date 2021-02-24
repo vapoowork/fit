1c17b812b678bd3b182c1220c37f7beded832819
Vasylii Puzyrev <vapoo.work@yandex.ru>
2021-02-24 23:52:40 +0300

    Добавлено модульное тестирование для контроллера пользователя

--- CodeBlogFitness.BL/Controller/UserController.cs
+++ CodeBlogFitness.BL/Controller/UserController.cs
@@ -54,7 +54,7 @@ namespace CodeBlogFitness.BL.Controller {
         private List<User> GetUsersData() {
             var formatter = new BinaryFormatter();
             using (var fs = new FileStream("users.dat", FileMode.OpenOrCreate)) {
-                if (formatter.Deserialize(fs) is List<User> users) {
+                if (fs.Length > 0 && formatter.Deserialize(fs) is List<User> users) {
                     return users;
                 } else {
                     return new List<User>();
---
+++ CodeBlogFitness.BLTests/Controller/UserControllerTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeBlogFitness.BL.Controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBlogFitness.BL.Controller.Tests {
+    [TestClass()]
+    public class UserControllerTests {
+
+        [TestMethod()]
+        public void SaveTest() {
+            // Arrange
+            var userName = Guid.NewGuid().ToString();
+
+            // Act
+            var controller = new UserController(userName);
+
+            // Assert
+            Assert.AreEqual(userName, controller.CurrentUser.Name);
+        }
+
+        [TestMethod()]
+        public void SetNewUserDataTest() {
+            // Arrange
+            var userName = Guid.NewGuid().ToString();
+            var birthDate = DateTime.Now.AddYears(-18);
+            var weigth = 67;
+            var height = 166;
+            var gender = "man";
+            var controller = new UserController(userName);
+            // Act
+            controller.SetNewUserData(gender, birthDate, weigth,height);
+            var controller2 = new UserController(userName);
+            // Assert
+            Assert.AreEqual(userName,controller2.CurrentUser.Name);
+            Assert.AreEqual(birthDate, controller2.CurrentUser.BirthDate);
+            Assert.AreEqual(weigth, controller2.CurrentUser.Weigth);
+            Assert.AreEqual(height, controller2.CurrentUser.Height);
+            Assert.AreEqual(gender, controller2.CurrentUser.Gender.Name);
+        }
+    }
+}
